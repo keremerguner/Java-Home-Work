f05055296f14f7658d17a5a2914a8a9a6379f09a
Kerem Erguner <73108947+keremerguner@users.noreply.github.com>
2021-05-15 09:05:05 +0300

    class and constructor

---
+++ homeWorkOne/src/homeWorkOne/Product.java
@@ -0,0 +1,15 @@
+package homeWorkOne;
+
+public class Product {
+	public Product(String name, int percent, String speaking) {
+		this.name = name;
+		this.percent = percent;
+		this.speaking = speaking;
+		
+	}
+	
+	String name;
+	int percent;
+	String speaking;
+	
+}
---
+++ homeWorkOne/src/homeWorkOne/ProductManager.java
@@ -0,0 +1,8 @@
+package homeWorkOne;
+
+public class ProductManager {
+	public void addToCourse(Product product) {
+		System.out.println("Secebilecegin kurslar = " + product.name);
+	}
+
+}
